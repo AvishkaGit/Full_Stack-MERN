0ae6e3dfeb9a0f666f076cf0fea0b8a86e758c8d
Avishka madhuranga <131765154+AvishkaGit@users.noreply.github.com>
2024-07-08 12:15:09 +0530

    update
    connect mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,31 @@
-const app = require('./app');
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const port = 3001;
 const host = '127.0.0.1';
+const mongoose = require('mongoose');
+const router = require('./router');
+
+
+app.use(cors());
+app.use(express.json());
+
+const uri = 'mongodb+srv://avishka:Colombo@1@cluster0.i2r5wnu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const connect = async () =>{
+    try{
+        await mongoose.connect(uri);
+        console.log('connected to mongodb');
+    }catch{
+        console.log('mongo db erro');
+    }
+}
+
+connect();
+
 
 const server = app.listen(port, host, () => {
     console.log(`Node server is listening to ${server.address().port}`)
 });
+
+app.use('/api', router);
